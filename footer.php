53462934d45b86343b1e2a1e08cd604b74fecc62
otherness-space <the@otherness.space>
2017-03-18 23:49:22 -0400

    actual underscores files

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Immaterialize
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'immaterialize' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'immaterialize' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'immaterialize' ), 'immaterialize', '<a href="https://automattic.com/" rel="designer">Rob Goodfellow</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
