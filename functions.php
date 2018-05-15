b8f683a57ef1fc5fe2e416c61a0d3cd10d8636fa
otherness-space <the@otherness.space>
2018-05-14 20:03:17 -0400

    edited links to show /assets/js/.. functions.php /inc/customizer.php README-underscores.md

--- functions.php
+++ functions.php
@@ -122,9 +122,9 @@ add_action( 'widgets_init', 'immaterialize_widgets_init' );
 function immaterialize_scripts() {
 	wp_enqueue_style( 'immaterialize-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'immaterialize-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'immaterialize-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'immaterialize-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'immaterialize-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -158,4 +158,3 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
