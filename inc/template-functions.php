b17d246ce13a8094ffc55ffbfae7d49317cda76b
otherness-space <the@otherness.space>
2018-05-16 12:19:59 -0400

    Completely revised and updated stock underscores as of 2018.05.16 Contains NO MATERIALIZE files Contains NO folder and file structure edits

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -1,8 +1,6 @@
 <?php
 /**
- * Custom functions that act independently of the theme templates
- *
- * Eventually, some of the functionality here could be replaced by core features.
+ * Functions which enhance the theme by hooking into WordPress
  *
  * @package Immaterialize
  */
@@ -14,14 +12,14 @@
  * @return array
  */
 function immaterialize_body_classes( $classes ) {
-	// Adds a class of group-blog to blogs with more than 1 published author.
-	if ( is_multi_author() ) {
-		$classes[] = 'group-blog';
-	}
-
 	// Adds a class of hfeed to non-singular pages.
 	if ( ! is_singular() ) {
 		$classes[] = 'hfeed';
+	}
+
+	// Adds a class of no-sidebar when there is no sidebar present.
+	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+		$classes[] = 'no-sidebar';
 	}
 
 	return $classes;
@@ -29,7 +27,7 @@ function immaterialize_body_classes( $classes ) {
 add_filter( 'body_class', 'immaterialize_body_classes' );
 
 /**
- * Add a pingback url auto-discovery header for singularly identifiable articles.
+ * Add a pingback url auto-discovery header for single posts, pages, or attachments.
  */
 function immaterialize_pingback_header() {
 	if ( is_singular() && pings_open() ) {
