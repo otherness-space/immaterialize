34ebe1f788034b283a0bc4de8f5e28477d377a34
otherness-space <the@otherness.space>
2018-05-19 14:25:16 -0400

    trivial update because git garbled something

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?
+<?php
 /**
  * Immaterialize functions and definitions
  *
@@ -6,6 +6,7 @@
  *
  * @package Immaterialize
  */
+
 
 if ( ! function_exists( 'immaterialize_setup' ) ) :
 	/**
@@ -244,6 +245,28 @@ add_action( 'wp_enqueue_scripts', 'materialize_scripts' );
 /**
  * http://crambler.com/how-to-implement-prism-js-syntax-highlighting-into-your-wordpress-site/
  */
+
+/** Function to add prism.css and prism.js to the site.
+	*Register prism.css file
+	*/
+	// Function to add prism.css and prism.js to the site
+function add_prism() {
+	// Register prism.css file
+	wp_register_style(
+	'prismCSS', // handle name for the style so we can register, de-register, etc.
+	get_stylesheet_directory_uri() . '/assetscss//prism.min.css' // location of the prism.css file
+	);
+	// Register prism.js file
+	wp_register_script(
+	'prismJS', // handle name for the script so we can register, de-register, etc.
+	get_stylesheet_directory_uri() . '/assets/js/prism.min.js' // location of the prism.js file
+	);
+	// Enqueue the registered style and script files
+	wp_enqueue_style('prismCSS');
+	wp_enqueue_script('prismJS');
+}
+
+add_action('wp_enqueue_scripts', 'add_prism');
 
 /**
  * Customizer additions.
