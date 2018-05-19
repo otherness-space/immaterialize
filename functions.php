4e8af612348a3b9565cc3f40db1e51df0b21bc54
otherness-space <the@otherness.space>
2018-05-19 14:43:49 -0400

    resolving conflict

--- functions.php
+++ functions.php
@@ -242,29 +242,6 @@ function materialize_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'materialize_scripts' );
 
-
-/** Function to add prism.css and prism.js to the site.
-	*Register prism.css file
-	*/
-	// Function to add prism.css and prism.js to the site
-function add_prism() {
-	// Register prism.css file
-	wp_register_style(
-	'prismCSS', // handle name for the style so we can register, de-register, etc.
-	get_stylesheet_directory_uri() . '/assetscss//prism.min.css' // location of the prism.css file
-	);
-	// Register prism.js file
-	wp_register_script(
-	'prismJS', // handle name for the script so we can register, de-register, etc.
-	get_stylesheet_directory_uri() . '/assets/js/prism.min.js' // location of the prism.js file
-	);
-	// Enqueue the registered style and script files
-	wp_enqueue_style('prismCSS');
-	wp_enqueue_script('prismJS');
-}
-
-add_action('wp_enqueue_scripts', 'add_prism');
-
 /**
  * Customizer additions.
  */
