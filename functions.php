ca14f31cbab39f205b4fe6554878a10ea74f36fd
otherness-space <the@otherness.space>
2018-05-21 15:15:24 -0400

    restoring lost php extensions in functions.php

--- functions.php
+++ functions.php
@@ -93,7 +93,7 @@ add_action( 'after_setup_theme', 'immaterialize_setup' );
 function immaterialize_content_width() {
 	// This variable is intended to be overruled from themes.
 	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
-	// cs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
 	$GLOBALS['content_width'] = apply_filters( 'immaterialize_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'immaterialize_content_width', 0 );
@@ -137,17 +137,17 @@ add_action( 'wp_enqueue_scripts', 'immaterialize_scripts' );
 /**
  * Implement the Custom Header feature.
  */
-require get_template_directory() . '/inc/custom-header.';
+require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
  */
-require get_template_directory() . '/inc/template-tags.';
+require get_template_directory() . '/inc/template-tags.php';
 
 /**
  * Functions which enhance the theme by hooking into WordPress.
  */
-require get_template_directory() . '/inc/template-functions.';
+require get_template_directory() . '/inc/template-functions.php';
 
 /**
  * Enqueue Materialize scripts and styles.
