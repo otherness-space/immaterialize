2c680e95100e5433e71581a606296a7f32b04908
otherness-space <the@otherness.space>
2018-09-13 19:29:42 -0400

    template-parts/content-page.php content box alignment snafu: fixed

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,47 +11,49 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php the_title('<h1 class="entry-title">', '</h1>'); ?>
 	</header><!-- .entry-header -->
 
 	<?php immaterialize_post_thumbnail(); ?>
 
 	<div class="entry-content">
 		<div class="row">
-			<div class="offset-s1 offset-m2dot5 offset-l1 col s10 m7 l6">
+			<div class="container justify-content display-flex">
+				<div class="col s9dot5 m9 l10 xl9dot5 content-box">
 		<?php
-		the_content();
+        the_content();
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'immaterialize' ),
-			'after'  => '</div>',
-		) );
-		?>
+        wp_link_pages(array(
+            'before' => '<div class="page-links">' . esc_html__('Pages:', 'immaterialize'),
+            'after'  => '</div>',
+        ));
+        ?>
+			</div>
 		</div>
 	</div>
 </div>
 <!-- .entry-content -->
 
-	<?php if ( get_edit_post_link() ) : ?>
+	<?php if (get_edit_post_link()) : ?>
 		<footer class="entry-footer">
 			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'immaterialize' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					get_the_title()
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
+            edit_post_link(
+                sprintf(
+                    wp_kses(
+                        /* translators: %s: Name of current post. Only visible to screen readers */
+                        __('Edit <span class="screen-reader-text">%s</span>', 'immaterialize'),
+                        array(
+                            'span' => array(
+                                'class' => array(),
+                            ),
+                        )
+                    ),
+                    get_the_title()
+                ),
+                '<span class="edit-link">',
+                '</span>'
+            );
+            ?>
 		</footer><!-- .entry-footer -->
 	<?php endif; ?>
 </article><!-- #post-<?php the_ID(); ?> -->
