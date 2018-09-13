a6d01c794bc2209d105dad75e9a50fe8de937d45
otherness-space <the@otherness.space>
2018-09-13 19:42:51 -0400

    template-parts/content-none.php content box alignment snafu: fixed

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -12,47 +12,53 @@
 <section class="no-results not-found">
 	<header class="page-header">
 		<div class="row">
-			<div class="offset-s1 offset-m2dot5 offset-l1 col s10 m7 l6">
-				<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'immaterialize' ); ?></h1>
+			<div class="container justify-content display-flex">
+				<div class="col s9dot5 m9 l10 xl9dot5 content-box">
+				<h1 class="page-title"><?php esc_html_e('Nothing Found', 'immaterialize'); ?></h1>
 			</div>
 		</div>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
 		<div class="row">
-			<div class="offset-s1 offset-m2dot5 offset-l1 col s10 m7 l6">
+			<div class="container justify-content display-flex">
+				<div class="col s9dot5 m9 l10 xl9dot5 content-box">
 		<?php
-		if ( is_home() && current_user_can( 'publish_posts' ) ) :
+        if (is_home() && current_user_can('publish_posts')) :
 
-			printf(
-				'<p>' . wp_kses(
-					/* translators: 1: link to WP admin new post page. */
-					__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'immaterialize' ),
-					array(
-						'a' => array(
-							'href' => array(),
-						),
-					)
-				) . '</p>',
-				esc_url( admin_url( 'post-new.php' ) )
-			);
+            printf(
+                '<p>' . wp_kses(
+                    /* translators: 1: link to WP admin new post page. */
+                    __('Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'immaterialize'),
+                    array(
+                        'a' => array(
+                            'href' => array(),
+                        ),
+                    )
+                ) . '</p>',
+                esc_url(admin_url('post-new.php'))
+            );
 
-		elseif ( is_search() ) :
-			?>
+        elseif (is_search()) :
+            ?>
 
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'immaterialize' ); ?></p>
+			<p><?php esc_html_e('Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'immaterialize'); ?></p>
 			<?php
 //			get_search_form();
 
-		else :
-			?>
+        else :
+            ?>
 			<div class="row">
-				<div class="offset-s1 offset-m2dot5 offset-l1 col s10 m7 l6">
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'immaterialize' ); ?></p>
+				<div class="container justify-content display-flex">
+					<div class="col s9dot5 m9 l10 xl9dot5 content-box">
+			<p><?php esc_html_e('It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'immaterialize'); ?></p>
 			<?php
-			get_search_form();
+            get_search_form();
 
-		endif;
-		?>
-	</div></div></div><!-- .page-content -->
+        endif;
+        ?>
+			</div>
+		</div>
+	</div>
+</div><!-- .page-content -->
 </section><!-- .no-results -->
