7a32f7e4522126fa845e4b32af9cdd075118cb55
otherness-space <the@otherness.space>
2018-09-15 19:26:01 -0400

    footer.php site-footer site-info added materialize grid cols boxes to align-center

--- footer.php
+++ footer.php
@@ -15,17 +15,23 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'immaterialize' ) ); ?>">
+			<div class="row">
+				<div class="container justify-content display-flex">
+					<div class="col s9dot5 m9 l10 xl9 center-align">
+			<a href="<?php echo esc_url(__('https://wordpress.org/', 'immaterialize')); ?>">
 				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'immaterialize' ), 'WordPress' );
-				?>
+                /* translators: %s: CMS name, i.e. WordPress. */
+                printf(esc_html__('Proudly powered by %s', 'immaterialize'), 'WordPress');
+                ?>
 			</a>
 			<span class="sep"> | </span>
 				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'immaterialize' ), 'immaterialize', '<a href="https://github.com/otherness-space/immaterialize">Rob Goodfellow</a>' );
-				?>
+                /* translators: 1: Theme name, 2: Theme author. */
+                printf(esc_html__('Theme: %1$s by %2$s.', 'immaterialize'), 'immaterialize', '<a href="https://github.com/otherness-space/immaterialize">Rob Goodfellow</a>');
+                ?>
+					</div>
+				</div>
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
